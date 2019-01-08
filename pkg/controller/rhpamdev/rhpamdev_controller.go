51f85bee78f5b500aa17b455b1392e21f60fd796
Bernard Tison <bernard.tison@gmail.com>
2019-01-08 04:35:33 +0100

    Cleaning up code

--- pkg/controller/rhpamdev/rhpamdev_controller.go
+++ pkg/controller/rhpamdev/rhpamdev_controller.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	gptev1alpha1 "github.com/gpte-naps/rhpam-dev-operator/pkg/apis/gpte/v1alpha1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -104,16 +103,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner RhpamDev
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &gptev1alpha1.RhpamDev{},
-	})
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
@@ -130,8 +119,6 @@ type ReconcileRhpamDev struct {
 
 // Reconcile reads that state of the cluster for a RhpamDev object and makes changes based on the state read
 // and what is in the RhpamDev.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
-// a Pod as an example
 // Note:
 // The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
